4d1e3009ba876440093dfb706c47d031f30b86c6
VityaMelnyk <vityamozer@gmail.com>
2020-09-22 21:11:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte myAge = 20;
+        System.out.println("my age is " + myAge + " years.");
+
+        short mySalary = 2280;
+        System.out.println("My expected is " + mySalary + "dolars");
+
+        int myAgeInHours = 177360;
+        System.out.println("My age is " + myAgeInHours + " in hours");
+
+        long myRangeFromSun = 147000000;
+        System.out.println("My distance to Sun is " + myRangeFromSun + " km.");
+
+            float myHeight = 174.90f;
+        System.out.println("My height is " + myHeight + " cm.");
+
+        double myWeight = 65.65656;
+        System.out.println("My weight is " + myWeight + " kg.");
+
+        boolean myGender = true;
+        System.out.println("My gender is " + myGender ".");
+
+        char myEnglishMark = 'C';
+        System.out.println("MyEnglishMark is " + myEnglishMark".");
+
+    }
+}
